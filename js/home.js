550897904abcd7baed06ab2bd7326afa7103a6cf
aanchalrakheja <aanchalrakheja2001@gmail.com>
2021-10-07 17:26:43 +0530

    updated

--- js/home.js
+++ js/home.js
@@ -1,33 +1,7 @@
-let tagline=document.getElementById("title-tagline");
-let text=document.getElementById("title-text");
+let tagline=document.getElementById("title-tagline-content");
+let cursorLine=document.getElementById("title-tagline-cursor");
 let taglineContent="ONE STOP RESOURCE GUIDE FOR UNSTOPPABLE LEARNING".split("");
-let textContent=" Start your self-learning journey with us! ";
 var i=0;
-let navbarIcon=document.getElementById("navbar-icon");
-let navMenu=document.getElementById("navbar-expand");
-var count=0;
-
-navbarIcon.addEventListener("click",function()
-{   
-    navbarIcon.classList.toggle("fa-bars");
-    navbarIcon.classList.toggle("fa-times");
-    if(count%2===0)
-    {
-    navMenu.style.display="flex";
-    navMenu.addEventListener("click",function(){
-        navMenu.style.display="none";
-        navbarIcon.classList.toggle("fa-times");
-        navbarIcon.classList.toggle("fa-bars");
-        count++;
-    });
-    }
-    else
-    {
-        navMenu.style.display="none";
-    }
-    count++;
-});
-
 
 function inputTagline()
 { 
@@ -39,13 +13,8 @@ function inputTagline()
     }
     else if(i===taglineContent.length)
     {
-        setTimeout(inputText,200);
+        cursorLine.style.display="none";
     }
-}
-
-function inputText()
-{
-    text.innerHTML=textContent;
 }
 
 inputTagline();
